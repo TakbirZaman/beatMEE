c4f77fab44f9ceecd209aa829ff0e390f9483531
Takbir Zaman Bhuiyan <takbirzamanbhuiyan@gmail.com>
2026-03-19 14:45:22 +0600

    add TURN servers for cross-network multiplayer

--- src/beatMEE.jsx
+++ src/beatMEE.jsx
@@ -1779,7 +1779,9 @@ export default function BeatMEE() {
           iceServers: [
             { urls: "stun:stun.l.google.com:19302" },
             { urls: "stun:stun1.l.google.com:19302" },
-            { urls: "stun:global.stun.twilio.com:3478" },
+            { urls: "turn:openrelay.metered.ca:80",       username: "openrelayproject", credential: "openrelayproject" },
+            { urls: "turn:openrelay.metered.ca:443",      username: "openrelayproject", credential: "openrelayproject" },
+            { urls: "turn:openrelay.metered.ca:443?transport=tcp", username: "openrelayproject", credential: "openrelayproject" },
           ]
         }
       });
@@ -1880,7 +1882,9 @@ export default function BeatMEE() {
           iceServers: [
             { urls: "stun:stun.l.google.com:19302" },
             { urls: "stun:stun1.l.google.com:19302" },
-            { urls: "stun:global.stun.twilio.com:3478" },
+            { urls: "turn:openrelay.metered.ca:80",       username: "openrelayproject", credential: "openrelayproject" },
+            { urls: "turn:openrelay.metered.ca:443",      username: "openrelayproject", credential: "openrelayproject" },
+            { urls: "turn:openrelay.metered.ca:443?transport=tcp", username: "openrelayproject", credential: "openrelayproject" },
           ]
         }
       });
